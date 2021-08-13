180b6575eddb3a7993fb5f7e1b0f032ab3d0a3ce
BlackShadw77 <39945757+BlackShadw77@users.noreply.github.com>
2021-08-13 11:15:35 +1000

    remove \ from end of file path

--- RestoreUser .bat
+++ RestoreUser .bat
@@ -1,9 +1,9 @@
 @ECHO OFF
-robocopy "%~dp0%USERNAME%\Desktop\" "%USERPROFILE%\Desktop\" /e /r:0
-robocopy "%~dp0%USERNAME%\Documents\" "%USERPROFILE%\Documents\" /e /r:0
-robocopy "%~dp0%USERNAME%\Downloads\" "%USERPROFILE%\Downloads\" /e /r:0
-robocopy "%~dp0%USERNAME%\Music\" "%USERPROFILE%\Music\" /e /r:0
-robocopy "%~dp0%USERNAME%\Videos\" "%USERPROFILE%\Videos\" /e /r:0
-robocopy "%~dp0%USERNAME%\Pictures\" "%USERPROFILE%\Pictures\" /e /r:0
+robocopy "%~dp0%USERNAME%\Desktop" "%USERPROFILE%\Desktop" /e /r:0
+robocopy "%~dp0%USERNAME%\Documents" "%USERPROFILE%\Documents" /e /r:0
+robocopy "%~dp0%USERNAME%\Downloads" "%USERPROFILE%\Downloads" /e /r:0
+robocopy "%~dp0%USERNAME%\Music" "%USERPROFILE%\Music" /e /r:0
+robocopy "%~dp0%USERNAME%\Videos" "%USERPROFILE%\Videos" /e /r:0
+robocopy "%~dp0%USERNAME%\Pictures" "%USERPROFILE%\Pictures" /e /r:0
 ECHO %USERNAME%'s files Restored.
 PAUSE
