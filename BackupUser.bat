68c0b1e6659d5078d0984ac09025ea5e5f67056d
BlackShadw77 <39945757+BlackShadw77@users.noreply.github.com>
2021-08-13 11:13:25 +1000

    Update BackupUser.bat

--- BackupUser.bat
+++ BackupUser.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
-robocopy "%USERPROFILE%\Desktop\" "%~dp0%USERNAME%\Desktop\" /e /r:0
-robocopy "%USERPROFILE%\Documents\" "%~dp0%USERNAME%\Documents\" /e /r:0
-robocopy "%USERPROFILE%\Downloads\" "%~dp0%USERNAME%\Downloads\" /e /r:0
-robocopy "%USERPROFILE%\Music\" "%~dp0%USERNAME%\Music\" /e /r:0
-robocopy "%USERPROFILE%\Videos\" "%~dp0%USERNAME%\Videos\" /e /r:0
-robocopy "%USERPROFILE%\Pictures\" "%~dp0%USERNAME%\Pictures\" /e /r:0
+robocopy "%USERPROFILE%\Desktop" "%~dp0%USERNAME%\Desktop" /e /r:0
+robocopy "%USERPROFILE%\Documents" "%~dp0%USERNAME%\Documents" /e /r:0
+robocopy "%USERPROFILE%\Downloads" "%~dp0%USERNAME%\Downloads" /e /r:0
+robocopy "%USERPROFILE%\Music" "%~dp0%USERNAME%\Music" /e /r:0
+robocopy "%USERPROFILE%\Videos" "%~dp0%USERNAME%\Videos" /e /r:0
+robocopy "%USERPROFILE%\Pictures" "%~dp0%USERNAME%\Pictures" /e /r:0
 ECHO %USERNAME%'s files backed Up to %~dp0%USERNAME%.
 PAUSE
